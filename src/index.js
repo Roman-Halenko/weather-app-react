80477e3f73c2ce531adb0fba6f94e604dd0a31a8
Roman Halenko <owlbro.popping@gmail.com>
2019-06-26 00:58:52 +0300

    Add weather icons

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import {render} from 'react-dom'
 import App from './js/components/App'
 import './sass/main.sass'
+import './css/weather-icons.min.css'
 
 render(<App/>, document.getElementById('app'))
